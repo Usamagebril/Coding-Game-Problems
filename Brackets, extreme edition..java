11fccee878f584de35a29cf2997448656add295f
Usama Gebril <63911614+Usamagebril@users.noreply.github.com>
2022-08-09 05:50:02 +0200

    Create  Brackets, extreme edition..java

---
+++ Brackets, extreme edition..java
@@ -0,0 +1,56 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        String expression = in.next();
+
+        Stack<Character> stack = new Stack<>();
+
+        boolean found = true;
+        for (Character c : expression.toCharArray()) {
+            switch (c) {
+                case '{':
+                case '(':
+                case '[':
+                    stack.push(c);
+                    break;
+                case '}':
+                    if (stack.isEmpty() || stack.pop() != '{') {
+                        found = false;
+                    }
+                    break;
+                case ')':
+                    if (stack.isEmpty() || stack.pop() != '(') {
+                        found = false;
+                    }
+                    break;
+                case ']':
+                    if (stack.isEmpty() || stack.pop() != '[') {
+                        found = false;
+                    }
+                    break;
+            }
+
+            if (!found) {
+                break;
+            }
+        }
+
+        if (found) {
+            found = stack.isEmpty();
+        }
+
+        System.out.println(found ? "true" : "false");
+
+        // Write an answer using System.out.println()
+        // To debug: System.err.println("Debug messages...");
+    }
+}
